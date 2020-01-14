84a0c7aa6c3c013c08d40a812f40cdd1280178c2
Eldar Shmatkov <eldarshmatkov@gmail.com>
2020-01-14 20:09:10 +0300

    Route reuse for saving page state

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, RouteReuseStrategy} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SingleUserComponent} from './single-user/single-user.component';
+import {CustomRouteReuseStategy} from './custom-route-reuse-strategy';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, data: { shouldReuse: true }},
   {path: 'single-user/:username', component: SingleUserComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: CustomRouteReuseStategy },
+  ]
 })
 export class AppRoutingModule { }
