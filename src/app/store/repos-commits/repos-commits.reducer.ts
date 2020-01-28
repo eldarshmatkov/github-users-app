87b72ec5b382837f613d3ae6e0fa297adf8e388b
Eldar Shmatkov <eldarshmatkov@gmail.com>
2020-01-28 12:49:28 +0300

    hotfix errors resolve

--- src/app/store/repos-commits/repos-commits.reducer.ts
+++ src/app/store/repos-commits/repos-commits.reducer.ts
@@ -1,18 +1,13 @@
 import * as ReposCommitsActions from './repos-commits.actions';
-import {UserCommitsResponse} from './userCommitsResponse.type';
 import {CustomAction} from '../custom-action.type';
-
-const initialState: UserCommitsResponse = {repo: '', items: []};
 
 export function
 reposCommitsReducer(state, action: ReposCommitsActions.CommitsLoaded | ReposCommitsActions.CommitsFailed)
-  : UserCommitsResponse | CustomAction {
+  : CustomAction {
   switch (action.type) {
     case ReposCommitsActions.COMMITS_LOADED:
       return action;
     case ReposCommitsActions.COMMITS_FAILED:
       return action;
-    default:
-      return initialState;
   }
 }
